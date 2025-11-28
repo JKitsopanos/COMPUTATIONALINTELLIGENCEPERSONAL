b230ad4da5bfc3c8ebb04248d473cab68439f0e4
Noah M. Tarr <noah@tarr.me.uk>
2025-11-28 02:38:33 +0000

    update DE

--- de.py
+++ de.py
@@ -3,39 +3,27 @@
 """Differential evolution with the original mutation scheme and binomial crossover (DE/rand/1/bin)."""
 
 import random
+from collections.abc import Iterable
 
 import torch
 
 import main
+import loss_function as lf  # terrible naming hack
+
 from main import CNN
 
-POPULATION_SIZE = 10
+POPULATION_SIZE = 4
 
-
-def model_to_individual(model: main.CNN) -> torch.Tensor:
-    return model.fc3.weight.flatten()
-
-
-def individual_to_model(individual: torch.Tensor, model: main.CNN) -> main.CNN:
-    with torch.no_grad():
-        model.fc3.weight[:] = individual.reshape(model.fc3.weight.shape)
-
-    return model
-
-
-def _loss_function(model: main.CNN):
-    return random.random()
-
-
-def loss_function(individual: torch.Tensor):
-    return _loss_function(individual_to_model(individual, base))
+base_model = torch.load("base.pt", weights_only=False)
+loss_function = lf.function(base_model)
 
 
 def generate_samples(model: main.CNN, n: int) -> torch.Tensor:
     assert n >= 4  # otherwise differential evolution cannot work
-    
+
+    # I retract the below comment.
     # This currently samples from a uniform distribution on [0,1); this should be replaced.
-    return torch.rand((n, *model_to_individual(model).shape))
+    return torch.rand((n, *lf.model_to_individual(model).shape))
 
 
 def mutate(original: torch.Tensor, mutant: torch.Tensor, differential_weight) -> None:
@@ -88,7 +76,16 @@ def select_survivors(original: torch.Tensor, offspring: torch.Tensor, f) -> None
             original[i] = offspring[i]
 
 
-def train(base: main.CNN, differential_weight, cr, population_size: int, steps: int) -> main.CNN:
+def get_best(population: Iterable[torch.Tensor]) -> torch.Tensor:
+    return min(population, key=loss_function)
+
+
+def train(base: main.CNN,
+          differential_weight,
+          cr,
+          population_size: int,
+          steps: int,
+          loss_modulus: int = 1) -> main.CNN:
     assert 0 <= cr <= 1
     assert population_size >= 4
     
@@ -96,16 +93,21 @@ def train(base: main.CNN, differential_weight, cr, population_size: int, steps: 
     mutant = torch.empty_like(population)
 
     for i in range(steps):
-        print(i)
+        print(i, end="\t")
+        if i % loss_modulus == 0:
+            print(loss_function(get_best(population)))
+        else:
+            print()
+            
         mutate(population, mutant, differential_weight)
         binomial_crossover(population, mutant, cr)
         select_survivors(population, mutant, loss_function)
 
-    return individual_to_model(min(population, key=loss_function), base)
+    return lf.individual_to_model(get_best(population), base)
 
 
 base = torch.load("base.pt", weights_only=False)
 
 if __name__ == "__main__":
     print(base)
-    print(train(base, 0.5, 0.5, 100, 5))
+    print(train(base, 0.5, 0.5, POPULATION_SIZE, 1))
